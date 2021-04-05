40be254570f7b2f78524fe67e809560e60317f1a
Jason <jasonjia@tamu.edu>
2021-04-04 22:20:21 -0500

    added POST and submit button. Ready to carry on and POST the data and do prediction

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, url_for,  flash, redirect
+from flask import Flask, render_template, url_for,  flash, redirect, request
+import flask
 from NBA_Stats_Scraper import NBA_Stats_Scraper
 from analysis import analysis, get_name, team_name
 app = Flask(__name__)
+# debug mode in flask:  export FLASK_DEBUG=1
+# export FLASK_APP=app
+# export FLASK_ENV=development
+# flask run
 
 nba = NBA_Stats_Scraper()
 team = team_name()
-print (team)
 # team_name = get_name()
 # print (type(team_name))
 
@@ -16,11 +20,12 @@ app.config['SECRET_KEY'] = 'ea7b11f0714027a81e7f81404612d80d'
 # this is the home page
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/home", methods=['GET', 'POST'])
-# debug mode in flask:  export FLASK_DEBUG=1
-# export FLASK_APP=app
-# export FLASK_ENV=development
-# flask run
 def home(): 
+    if request.method == "POST":
+        teamone = request.form.get("teamApic", None)
+        if teamone!=None:
+            return render_template("home.html", teamone = teamone, team = team)  
+# ready to create 2 submit button and a predict button for POST. Submit button would use dynamic image, and the predict button will POST
     return render_template("home.html", team = team)
 
 
